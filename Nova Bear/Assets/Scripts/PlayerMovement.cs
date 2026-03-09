8354fc37227aa764d23aa0486ea2963f7a7f7718
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2026-03-09 11:41:13 -0700

    Added move script to player ship. Moves on update by checking the value of the moveShipAction defined in the player actions input asset. Used a vector3 for efficiency rather than creating a new vector every frame.

--- Nova Bear/Assets/Scripts/PlayerMovement.cs
+++ Nova Bear/Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Only needed if not attached to player ship. In that case, set this to the
+    // player ship object.
+    [SerializeField] private GameObject PlayerShip;
+    // Controls used for ship.
+    [SerializeField] private InputActionAsset PlayerControls;
+    // Helper to get the Player actions.
+    private InputAction _moveShipAction;
+    // How fast thie ship moves.
+    [SerializeField] private float _shipMoveRate = 10f;
+    // Helps move without creating new Vector3 every frame to help with efficiency and clarity.
+    private Vector3 shipMovePosition;
+    private void Awake()
+    {
+        PlayerControls.Enable();
+        _moveShipAction = PlayerControls.FindActionMap("Player")["Move"];
+        shipMovePosition = Vector3.zero;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,12 +30,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void OnMove(InputValue value)
-    {
-        Debug.Log(value.Get<Vector2>());
+        shipMovePosition.x = _shipMoveRate * _moveShipAction.ReadValue<Vector2>().x * Time.deltaTime;
+        shipMovePosition.y = _shipMoveRate * _moveShipAction.ReadValue<Vector2>().y * Time.deltaTime;
+        transform.localPosition += shipMovePosition;
     }
     
+    // Current move rate of the ship.
+    public float GetMoveRate()
+    {
+        return _shipMoveRate;
+    }
+
+    // Set new move rate of the ship (power ups? power downs?)
+    public void SetMoveRate(float newRate)
+    {
+        _shipMoveRate = newRate;
+    }
+
 }
